daeed87d16a1317733ed5623cc235444f8852417
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2017-04-20 19:22:40 +0900

    ログファイル削除

--- fuel/app/logs/2016/03/01.php
+++
@@ -1,7 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2016-03-01 20:56:04 --> Parsing Error - syntax error, unexpected '&' in /mnt/fuelphp/fuel/app/classes/auth/login/baseauth.php on line 23
-ERROR - 2016-03-01 21:22:31 --> Fatal Error - Class Auth_Login_BaseAuth contains 1 abstract method and must therefore be declared abstract or implement the remaining methods (Auth\Auth_Login_Driver::get_email) in /mnt/fuelphp/fuel/app/classes/auth/login/baseauth.php on line 157
-ERROR - 2016-03-01 21:23:25 --> Parsing Error - syntax error, unexpected '=', expecting ')' in /mnt/fuelphp/fuel/app/classes/auth/group/baseauth.php on line 4
-ERROR - 2016-03-01 21:25:29 --> Parsing Error - syntax error, unexpected ';' in /mnt/fuelphp/fuel/app/classes/auth/acl/baseauth.php on line 4
-ERROR - 2016-03-01 21:27:45 --> Parsing Error - syntax error, unexpected ';' in /mnt/fuelphp/fuel/app/classes/auth/acl/baseauth.php on line 4
